d74d3a84828ee22997a843665afe0eace71fa4ea
Bartek Ciszewski <bartektricks@gmail.com>
2021-01-19 23:18:20 +0100

    Create basic gql CRUD.

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,21 +1,43 @@
+import "reflect-metadata";
 import { MikroORM } from "@mikro-orm/core";
+import express from "express";
+import { ApolloServer } from "apollo-server-express";
+import { buildSchema } from "type-graphql";
 
 import { __prod__ } from "./constants";
-import { Post } from "./entities/Post";
-import microConfig from './mikro-orm.config';
+import { HelloResolver } from "./resolvers/Hello";
+import { PostResolver } from "./resolvers/Post";
+import microConfig from "./mikro-orm.config";
 
-require('dotenv').config();
+require("dotenv").config();
+
+const PORT = process.env.PORT || 2137;
 
 const main = async () => {
   const orm = await MikroORM.init(microConfig);
-
   await orm.getMigrator().up();
 
-  const post = orm.em.create(Post, { title: 'My first post' });
-  
-  await orm.em.persistAndFlush(post);
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [HelloResolver, PostResolver],
+      validate: false,
+    }),
+    context: () => ({
+      em: orm.em,
+    }),
+  });
+
+  apolloServer.applyMiddleware({ app });
+
+  app.get("/", (_, res) => {
+    res.send("Hello");
+  });
+
+  app.listen(PORT, () => {
+    console.log(`Server started on port http://localhost:%d`, PORT);
+  });
 };
 
-main().catch(err => {
-  console.log(err)
-});
+main();
